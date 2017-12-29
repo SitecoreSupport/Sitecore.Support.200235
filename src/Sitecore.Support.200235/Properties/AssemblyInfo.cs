d6d2d3afee2822f9e5a7e5bce72ea98b0c670cbe
DK\DMRO <dmro@sitecore.net>
2017-12-29 16:09:11 +0200

    Initial code

---
+++ src/Sitecore.Support.200235/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.200235")]
+[assembly: AssemblyProduct("Sitecore.Support.200235")]
+[assembly: ComVisible(false)]
